71237e8017d0e65bb0e7f1c5496d98a754b80f65
xennvoid <thundvgod@gmail.com>
2024-05-23 20:00:15 +0300

    update routes and add routes type

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,17 @@
 import Home from './pages/Home';
+import QuizCreation from './pages/QuizCreation';
 
 const ROUTES = {
     HOME: {
-        path: '/',
+        path: '/' as const,
         element: Home,
     },
+    QUIZ_CREATION: {
+        path: '/create' as const,
+        element: QuizCreation,
+    },
 };
+
+export type IPath = (typeof ROUTES)[keyof typeof ROUTES]['path'];
 
 export default ROUTES;
