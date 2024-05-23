f656c08307f599f94e81ea02440432641a59c3d1
xennvoid <thundvgod@gmail.com>
2024-05-23 17:20:12 +0300

    fix tailwind and vite configs

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,7 +3,12 @@ import { FC } from 'react';
 interface HomeProps {}
 
 const Home: FC<HomeProps> = ({}) => {
-    return <>Home</>;
+    return (
+        <>
+            <h1 className="text-5xl font-bold text-red-600">Quiz App</h1>
+            <span>123</span>
+        </>
+    );
 };
 
 export default Home;
